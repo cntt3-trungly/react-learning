395f32c5866d673bbce2f3c18bed98bebfb01fa9
trungly-cntt3 <handoilaanmay@gmail.com>
2019-06-21 21:39:34 +0700

    [PHAN-34] 099 Su dung toan tu ba cham

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // var p1 = {
+  //   gia : 100 ,
+  //   ten :"manhinh"
+  // }
+  // console.log(p1);
+  // var p2 = {...p1}
+  // p2.gia = 90;
+
+  // console.log(p2);
+  // console.log(p1);
+  
+  var a1 =[0,2,4];
+  var a2 = [...a1]  ;
+  a2[0]= 100;
+  console.log(a1);
+  console.log(a2);
+  
+  
+  
   return (
     <div className="App">
       <header className="App-header">
