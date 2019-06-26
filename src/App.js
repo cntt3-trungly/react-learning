e83733f4f95fdf850c2c9f897abab5051945efb1
trungly-cntt3 <handoilaanmay@gmail.com>
2019-06-26 20:24:59 +0700

    [PHAN-41] 117 Ket noi Reactjs-Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {firebaseConnect} from './firebaseConnect'
 
 function App() {
   return (
+    console.log(firebaseConnect),
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
---
+++ src/firebaseConnect.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+var firebaseConfig = {     
+apiKey : "AIzaSyDE7LEjflOms78ARW2XZuZfAbCvEIKGuBw" ,     
+authDomain : "notereactfedu-54730.firebaseapp.com" ,     
+DatabaseURL : "https://notereactfedu-54730.firebaseio.com " ,
+projectId : "notereactfedu-54730" ,     
+storageBucket : "" ,     
+messagingSenderId : "923870426890" ,     
+appId : "1: 923870426890: web: 97219ade4171f230" }; 
+ export const firebaseConnect =  firebase . initializeApp ( firebaseConfig );
+
+ 
