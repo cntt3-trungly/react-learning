2f02ccb55d510e591ff0bb641b6a9092dc3d9a72
trungly-cntt3 <handoilaanmay@gmail.com>
2019-06-26 18:27:34 +0700

    [PHAN-38] 111 Ham mapDispatchToProps

--- src/News.js
+++ src/News.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 class News extends Component {
-    useEditStatusInStore = () => {
-        var {dispatch} = this.props;
-        dispatch({
-            type:'CHANGE_EDIT_STATUS'
-        })
-    }
+    // useEditStatusInStore = () => {
+    //     var {dispatch} = this.props;
+    //     dispatch({
+    //         type:'CHANGE_EDIT_STATUS'
+    //     })
+    // }
     render() {
 
         return (
             <div>
                 <p>Đây là Component News</p>
-                <button onClick={() =>this.useEditStatusInStore()}>Click đi</button>
+                <button onClick={() =>this.props.useEditStatusInStore()}>Click đi</button>
             </div>
         );
     }
@@ -23,4 +23,11 @@ const mapStateToProps = (state, ownProps) => {
         editStatus: state.editStatus
     }
 }
-export default connect(mapStateToProps)(News);
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        useEditStatusInStore: () => {
+            dispatch({type:'CHANGE_EDIT_STATUS'})
+        }
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(News);
