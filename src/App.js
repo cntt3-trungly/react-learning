9dd562b6d1507d84d7c053821d38e599cca7f379
trungly-cntt3 <handoilaanmay@gmail.com>
2019-06-26 17:08:46 +0700

    [PHAN-38] 108 Truy xuat du lieu trong store tu component con

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import News from './News';
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
     return (
       <div>
+        {this.props.dulieu}
         <News/>
       </div>
     );
   }
 }
+const mapStateToProps = (state, ownProps) => {
+  return {
+    dulieu : state.num
+  }
+}
 
-export default App;
+export default connect(mapStateToProps)(App);
