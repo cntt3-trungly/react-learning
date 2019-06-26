b664620f63512ca23cb5d75ce54181cff0794a34
trungly-cntt3 <handoilaanmay@gmail.com>
2019-06-26 15:39:04 +0700

    [PHAN-34] 102 Chay thu cau truc reducer_store_action

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    var redux = require('redux');
+    var oldState= {
+      num :["man hinh","ban phim","chuot"],
+      editStatus: true
+    }
+    var reducer1 =(state=oldState,action)=>{
+      switch (action.type) {
+        case "CHANGE_EDIT_STATUS":
+          return {...state,editStatus:!state.editStatus}          
+          break;
+      
+        default:
+          break;
+      }
+      return state;
+    }
+    var store1 = redux.createStore(reducer1);
+    store1.dispatch({type:"CHANGE_EDIT_STATUS"});
+    console.log(store1.getState());
+    
+
+    return (
+      <div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
