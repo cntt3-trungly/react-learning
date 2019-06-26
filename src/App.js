35c52871c1b0020610925b3fd31470c96e0e3ae4
trungly-cntt3 <handoilaanmay@gmail.com>
2019-06-26 15:53:44 +0700

    [PHAN-36] 103 Thuc hanh voi Redux va ham Subscribe

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  
+
   render() {
     var redux = require('redux');
-    var oldState= {
-      num :["man hinh","ban phim","chuot"],
+    var oldState = {
+      num: ["man hinh", "ban phim", "chuot"],
       editStatus: true
     }
-    var reducer1 =(state=oldState,action)=>{
+    var reducer1 = (state = oldState, action) => {
       switch (action.type) {
         case "CHANGE_EDIT_STATUS":
-          return {...state,editStatus:!state.editStatus}          
+          return { ...state, editStatus: !state.editStatus }
           break;
-      
+        case "ADD_NEW":
+          return { ...state, num:[...state.num,action.phantumoi] }
+          break;
+
         default:
           break;
       }
       return state;
     }
     var store1 = redux.createStore(reducer1);
-    store1.dispatch({type:"CHANGE_EDIT_STATUS"});
+    store1.dispatch({ type: "CHANGE_EDIT_STATUS" });
+    store1.dispatch({
+      type:"ADD_NEW",
+      phantumoi:"Tai Nghe"
+    })
     console.log(store1.getState());
     
 
+
     return (
       <div>
-        
+
       </div>
     );
   }
