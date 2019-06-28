f15f6205b0dcb69f3e4a6f7ab7d6edac4544d95a
trungly-cntt3 <handoilaanmay@gmail.com>
2019-06-28 15:29:09 +0700

    [PHAN-56] 160 Ket noi cac Component trong React JS

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import HeadTitle from './HeadTitle';
 import Products from './Product';
-import  './App.css';
+import './App.css';
 
 
 class App extends Component {
@@ -11,10 +11,15 @@ class App extends Component {
         <HeadTitle />
         <div className="container">
           <div className="row">
-            <Products/>
-            <Products/>
-            <Products/>
-            <Products/>
+            <Products
+              product_name='Bút chì'
+              product_price='500000'
+              image='http://channel.mediacdn.vn/2019/6/28/photo-1-15616990644991682841763.jpg' />
+            <Products
+              product_name='Bút bi'
+              product_price='100000'
+              image='https://kenh14cdn.com/zoom/280_175/2019/6/28/gif-4-15616969719491971969447-crop-15617045186841915171446.gif' />
+
           </div>
         </div>
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -5,10 +5,10 @@ class Products extends Component {
         return (
             <div className="col-4">
                 <div className="card text-left">
-                    <img className="card-img-top" src="http://thuthuat123.com/uploads/2018/01/27/anh-dai-dien-dep-nhat-10_091855.jpg" alt />
+                    <img className="card-img-top" src={this.props.image} alt="" />
                     <div className="card-body">
-                        <h5 className="card-title float-left">Tên sản phẩm</h5>
-                        <h5 className="card-title  float-right">500.000</h5>
+                        <h5 className="card-title float-left">{this.props.product_name}</h5>
+                        <h5 className="card-title  float-right">{this.props.product_price}</h5>
                     </div>
                 </div>
             </div>
